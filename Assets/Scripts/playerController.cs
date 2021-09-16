4417c161c29daba88b3f00d883d8c81be4d52f7c
gonzaloRamiaGarcia <al394793@uji.es>
2021-09-16 13:52:50 +0200

    Step 6 & 7 completed

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class playerController : MonoBehaviour
 {
 
     public float speed = 0.0f;
+    private int counter;
+    public TextMeshProUGUI countText;
+    public GameObject win;
 
     private Rigidbody rb;
     private float movementX;
@@ -16,6 +20,9 @@ public class playerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        counter = 0;
+        SetCountText();
+        win.SetActive(false);
     }
 
     // Update is called once per frame
@@ -24,11 +31,32 @@ public class playerController : MonoBehaviour
         Vector2 movementVector = movementValue.Get<Vector2>();
         movementX = movementVector.x;
         movementY = movementVector.y;
-}
+    }
 
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, .0f, movementY);
-        rb.AddForce(movement*speed);
+        rb.AddForce(movement * speed);
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            counter += 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + counter.ToString();
+        if (counter >= 12)
+        {
+            win.SetActive(true);
+        }
+
+    }
+
 }
